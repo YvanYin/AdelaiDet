14d50301fb68df089a2bb0c208657a3fe32ae627
Yin <Yinwi@147dda75a71d.ant.amazon.com>
2021-09-17 12:42:25 +0930

    add new train scripts

--- adet/data/__init__.py
+++ adet/data/__init__.py
@@ -1,5 +1,5 @@
 from . import builtin  # ensure the builtin datasets are registered
 from .dataset_mapper import DatasetMapperWithBasis
-
+from . import objects365_register
 
 __all__ = ["DatasetMapperWithBasis"]
